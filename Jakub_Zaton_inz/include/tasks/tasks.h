0f225f468d19c71fd25ef9fd872dff3057f6c4fc
Jakub Zatoń (253132) <253132@student.pwr.edu.pl>
2022-10-25 19:30:32 +0200

    files changed for inz

--- Jakub_Zaton_inz/include/tasks/tasks.h
+++ Jakub_Zaton_inz/include/tasks/tasks.h
@@ -8,15 +8,11 @@
 #include "../components/SDcard.h"
 #include <HX711_ADC.h>
 #include "../components/hx711.h"
-#include "../structs/stateMachine.h"
 #include "../com/internalCommunication.h"
 #include "../structs/commStructs.h"
-#include "../com/now.h"
-#include <MCP23017.h>
+
 
 extern SoftwareToolsManagment stm;
-extern InternalI2C<PWRData, TxData> pwrCom;
-extern MCP23017 expander;
 extern Hx711 rckWeight;
 extern Hx711 tankWeight;
 
@@ -27,10 +23,7 @@ void rxHandlingTask(void *arg);
 
 
 //app_cpu
-void sdTask(void *arg);
+// void sdTask(void *arg);
 void dataTask(void *arg);
-void stateTask(void *arg);
-void buzzerTask(void *arg);
-
 
 #endif
